8b5fa77c1bf4de56bfe1abe6c23fbecafce30160
Lisa Rees <kortirion@gmail.com>
2018-10-22 12:51:55 -0400

    slideshow

--- function.js
+++ function.js
@@ -24,3 +24,33 @@ navButton.addEventListener("click", function(){
         navLinks.style.display = "flex";
     }
 })
+
+var bioModal = document.getElementById('bioModal');
+var bioBtn = document.getElementById("bioBtn");
+var span = document.getElementsByClassName("close")[0];
+bioBtn.onclick = function() {
+    bioModal.style.display = "block";
+}
+span.onclick = function() {
+    bioModal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == bioModal) {
+        bioModal.style.display = "none";
+    }
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 5000); 
+}
